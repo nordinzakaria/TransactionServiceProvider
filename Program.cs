d6c69f45552e0641e82dd94c51e106473bb436d5
nordinzakaria <nordinzakaria@gmail.com>
2024-10-18 17:15:21 +0800

    a bit more

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
 
 using TransactionPackage;
+// need to install Firebaseadmin and Google.Cloud.Firestore sdk
 
 var builder = WebApplication.CreateBuilder(args);
+
+// NEED THIS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed(origin => true) // allow any origin
+            .AllowCredentials();
+        });
+});
+
+
 var app = builder.Build();
 
 Business buss = new Business();
+buss.initFirestore();
 
           // route
 app.MapGet("/",     () => "Hello World!");
@@ -13,15 +31,15 @@ app.MapGet("/timetable", () => "No class today");
 
 app.MapGet("/dinner", () => "No food today");
 
-app.MapGet("/", async c =>
+app.MapGet("/transactions", async c =>
 {
     await buss.RestoreTransactions();
 
-    c.Response.WriteAsJsonAsync(buss.TransList);
+    c.Response.WriteAsJsonAsync(buss.TransList.transactions);
 });
 
 
-app.MapPost("/addedit", async (Transaction trans) =>
+app.MapPost("/transactions/addedit", async (Transaction trans) =>
 {
     await buss.SaveTransaction(trans);
 
@@ -29,13 +47,14 @@ app.MapPost("/addedit", async (Transaction trans) =>
 });
 
 
-app.MapPost("/delete", async (Foo data) =>
+app.MapPost("/transactions/delete", async (Foo data) =>
 {
     await buss.DelTransaction(data.Id);
 
     return Results.NoContent();
 });
 
-
+// NEED THIS
+app.UseCors("AllowAll");
 
 app.Run();
